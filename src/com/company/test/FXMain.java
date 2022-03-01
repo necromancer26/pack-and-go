15b45a48912e30c614c41cea2c6766ee78e5a39f
khalil <khalil.chettaoui06@gmail.com>
2022-03-01 20:32:26 +0100

    javafx

---
+++ src/com/company/test/FXMain.java
@@ -0,0 +1,15 @@
+package com.company.test;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FXMain extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
